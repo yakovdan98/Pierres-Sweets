b8fe48cca8e46e960c73c8bbddcafae328ca76ba
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2023-01-13 15:43:02 -0800

    added treats controller

---
+++ SweetShop/Controllers/TreatsController.cs
@@ -0,0 +1,113 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using SweetShop.Models;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace SweetShop.Controllers
+{
+  public class TreatsController : Controller
+  {
+    // Routes
+    private readonly SweetShopContext _db;
+    public TreatsController(SweetShopContext db)
+    {
+      _db = db;
+    }
+    [AllowAnonymous]
+    public ActionResult Index()
+    {
+      List<Treat> treats = _db.Treats.ToList();
+      return View(treats);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Treat treat)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(treat);
+      }
+      else
+      {
+        _db.Treats.Add(treat);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+
+    }
+    [AllowAnonymous]
+    public ActionResult Details(int id)
+    {
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      Treat thisTreat = _db.Treats
+        .Include(joinEntry => joinEntry.TreatFlavors)
+        .ThenInclude(entity => entity.Flavor)
+        .FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Treat treat)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(treat);
+      }
+      else
+      {
+        _db.Treats.Update(treat);
+        _db.SaveChanges();
+        return RedirectToAction("Details", "Treats", new { id = treat.TreatId });
+      }
+    }
+
+    //     [HttpPost]
+    //     public ActionResult Delete(int id)
+    //     {
+    //       Author thisAuthor = _db.Authors.FirstOrDefault(author => author.AuthorId == id);
+    //       _db.Authors.Remove(thisAuthor);
+    //       _db.SaveChanges();
+    //       return RedirectToAction("Index");
+    //     }
+
+    //     public ActionResult AddBook(int id)
+    //     {
+    //       Author thisAuthor = _db.Authors.FirstOrDefault(entry => entry.AuthorId == id);
+    //       ViewBag.BookId = new SelectList(_db.Books, "BookId", "Title");
+    //       return View(thisAuthor);
+    //     }
+
+    [HttpPost]
+    public ActionResult AddFlavor(Treat treat, int flavorId)
+    {
+      // check if already an AuthorBook association
+#nullable enable
+      TreatFlavor? entry = _db.TreatFlavors.FirstOrDefault(entry => (entry.TreatId == treat.TreatId && entry.FlavorId == flavorId));
+#nullable disable
+      // if no AuthorBook association, create one
+      if (entry == null && flavorId != 0)
+      {
+        _db.TreatFlavors.Add(new TreatFlavor { TreatId = treat.TreatId, FlavorId = flavorId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", "Treats", new { id = treat.TreatId });
+    }
+
+
+  }
+}
